e26bd7a93de8a8598b246a8097d343cc63480a2f
harshp <parasharharsh16@gmail.com>
2025-02-16 21:13:55 +0530

    adding files for server and client

---
+++ client.py
@@ -0,0 +1,12 @@
+import requests
+
+server_url = 'http://192.168.56.101:8080/'
+
+try:
+    response = requests.get(server_url)
+    if response.status_code == 200:
+        print(f"Server says: {response.json()['message']}")
+    else:
+        print(f"Failed to connect. Status code: {response.status_code}")
+except requests.exceptions.RequestException as e:
+    print(f"Error connecting to the server: {e}")
